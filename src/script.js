061524db7f42f1bc41402b0963887de7e0edd4eb
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-24 12:09:03 +0530

    js todo

--- src/script.js
+++ src/script.js
@@ -1,73 +1,110 @@
-var incompleteTasksHolder = $('#incomplete-tasks');
-var completedTasksHolder = $('#completed-tasks');
-var editTask = function () {
-  var listItem = $(this).parent();
-  var listTextInput = listItem.find('input[type="text"]');
-  var listLabel = listItem.find('label');
-  if ( listItem.hasClass('editMode') ) {
-    listLabel.text( listTextInput.val() );
-    listTextInput.removeAttr('value');
-    listItem.removeAttr('class');
-    $(this).text('Edit');
-  } else {    
-    listTextInput.val( listLabel.text() );
-    listItem.addClass('editMode');
-    $(this).text('Save');
-  }
-};
+var taskInput = document.getElementById("new-task"); //new-task
+var addButton = document.getElementsByTagName("button")[0]; //first button
+var incompleteTasksHolder = document.getElementById("incomplete-tasks"); //incomplete-tasks
+var completedTasksHolder = document.getElementById("completed-tasks"); //completed-tasks
 
-var deleteTask = function () {
-  $(this).parent().remove();
-};
+//New Task List Item
+var createNewTaskElement = function(taskString) {
+	//Create List Item
+	var listItem = document.createElement("li");
 
-var addTask = function() {
-  var taskInput = $('#new-task');
-  if ( taskInput.val() == '' ) {
-    alert('You Can\'t add a Empty Task');
-  } else {
-    var listItem = $('<li><input type="checkbox"><label>' + taskInput.val() + '</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>');
-    incompleteTasksHolder.append(listItem);
-    taskInput.val('');
-    
-    var newTaskAdded = incompleteTasksHolder.find('li').last();
-    newTaskAdded.find('.edit').on('click', editTask);
-    newTaskAdded.find('.delete').on('click', deleteTask);
-    newTaskAdded.find('input[type="checkbox"]').on('click', markTask);
-  }
-};
+	//input (checkbox)
+	var checkBox = document.createElement("input"); // checkbox
+	//label
+	var label = document.createElement("label");
+	//input (text)
+	var editInput = document.createElement("input"); // text
+	//button.edit
+	var editButton = document.createElement("button");
+	//button.delete
+	var deleteButton = document.createElement("button");
 
-// ---  Function for Marking a task Complete or Incomplete ---
-var markTask = function() {
-  //This function will check if the task is in edit mode, if it is true, it will change it
-  function checkEditState(task) {
-    if ( task.hasClass('editMode') ) {  
-      task.find('label').text( task.find('input[type="text"]').val() )
-      task.find('input[type="text"]').removeAttr('value');
-      task.removeAttr('class');
-      task.find('.edit').text('Edit');
-    }
-  }
-  
-  if ( $(this).closest('ul').attr('id') == 'incomplete-tasks' ) {
-    var completedTask = $(this).parent().remove();
-    checkEditState(completedTask);
-    completedTask.find('input[type="checkbox"]').prop( "checked", true );    
-    bindTaskEvents( completedTask );
-    completedTasksHolder.append(completedTask);
-  } else {
-    var taskToComplete = $(this).parent().remove();
-    checkEditState(taskToComplete);
-    taskToComplete.find('input[type="checkbox"]').removeAttr('checked');
-    bindTaskEvents( taskToComplete );
-    incompleteTasksHolder.append(taskToComplete);
-  }
-};
-function bindTaskEvents (item) {
-  item.find('.edit').on('click', editTask);
-  item.find('.delete').on('click', deleteTask);
-  item.find('input[type="checkbox"]').on('click', markTask);
+	checkBox.type = "checkbox";
+	editInput.type = "text";
+
+	editButton.innerText = "Edit";
+	editButton.className = "edit";
+	deleteButton.innerText = "Delete";
+	deleteButton.className = "delete";
+
+	label.innerText = taskString;
+	listItem.appendChild(checkBox);
+	listItem.appendChild(label);
+	listItem.appendChild(editInput);
+	listItem.appendChild(editButton);
+	listItem.appendChild(deleteButton);
+
+	return listItem;
 }
 
-// Binding Event handlers to Add, checkboxes, edit and delete
-$('#new-task').next().on('click', addTask);
-bindTaskEvents( $('li') );
+//Add a new task
+var addTask = function() {
+	console.log("Add task...");
+	var listItem = createNewTaskElement(taskInput.value);
+	incompleteTasksHolder.appendChild(listItem);
+	bindTaskEvents(listItem, taskCompleted);
+
+	taskInput.value = "";
+}
+
+//Edit an existing task
+var editTask = function() {
+	console.log("Edit task...");
+
+	var listItem = this.parentNode;
+
+	var editInput = listItem.querySelector("input[type=text");
+	var label = listItem.querySelector("label");
+
+	var containsClass = listItem.classList.contains("editMode");
+	if (containsClass) {
+		label.innerText = editInput.value;
+	} else {
+		editInput.value = label.innerText;
+	}
+	listItem.classList.toggle("editMode");
+
+}
+
+//Delete an existing task
+var deleteTask = function() {
+	console.log("Delete task...");
+	var listItem = this.parentNode;
+	var ul = listItem.parentNode;
+	ul.removeChild(listItem);
+}
+
+//Mark a task as complete
+var taskCompleted = function() {
+	console.log("Task complete...");
+	var listItem = this.parentNode;
+	completedTasksHolder.appendChild(listItem);
+	bindTaskEvents(listItem, taskIncomplete);
+}
+
+//Mark a task as incomplete
+var taskIncomplete = function() {
+	console.log("Task incomplete...");
+	var listItem = this.parentNode;
+	incompleteTasksHolder.appendChild(listItem);
+	bindTaskEvents(listItem, taskCompleted);
+}
+
+var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
+	console.log("Bind list item events");
+	var checkBox = taskListItem.querySelector("input[type=checkbox]");
+	var editButton = taskListItem.querySelector("button.edit");
+	var deleteButton = taskListItem.querySelector("button.delete");
+	editButton.onclick = editTask;
+	deleteButton.onclick = deleteTask;
+	checkBox.onchange = checkBoxEventHandler;
+}
+addButton.addEventListener("click", addTask);
+for (var i = 0; i < incompleteTasksHolder.children.length; i++) {
+	bindTaskEvents(incompleteTasksHolder.children[i], taskCompleted);
+}
+
+
+for (var i = 0; i < completedTasksHolder.children.length; i++) {
+	bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+}
